47e798a9e6be655f7688e04cae046f27917a7861
Phyo Yarzar <phyoyazar592@gmail.com>
2022-07-20 22:31:15 +0630

    update all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,12 @@
+import EventList from '../../components/events/event-list';
+import { getAllEvents } from '../../dummy-data';
+
 const AllEventsPage = () => {
+  const allEvents = getAllEvents();
+
   return (
     <div>
-      <h1>All Events Page</h1>
+      <EventList items={allEvents} />;
     </div>
   );
 };
