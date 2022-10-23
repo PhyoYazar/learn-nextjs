835c75d5a24de1a1ff17b9ff2e220dd3604b806b
Phyo Yarzar <phyoyazar592@gmail.com>
2022-10-23 18:10:11 +0630

    [Head]adding head Content for Good SEO

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import Head from "next/head";
 import EventList from "../components/events/event-list";
 import { getFeaturedEvents } from "../components/helpers/api-util";
 
 const HomePage = (props) => {
   return (
     <div>
+      <Head>
+        <title>Nextjs Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events in here ..."
+        />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
